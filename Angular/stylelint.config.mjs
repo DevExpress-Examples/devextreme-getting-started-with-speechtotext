05fc58075cdbf6a5103573111dd600754ed4c20f
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-25 17:28:35 +0300

    Fix ESLint 9 migration: remove stale config files, add Angular stylelint.config.mjs and type:module

---
+++ Angular/stylelint.config.mjs
@@ -0,0 +1,7 @@
+import stylelintrc from 'eslint-config-devextreme/stylelintrc';
+
+export default {
+  extends: ['stylelint-config-standard-scss'],
+  ...stylelintrc,
+  overrides: [{ files: ['**/*.scss'], customSyntax: 'postcss-scss' }],
+};
