15087768f3a38042abe1fb287a7be39d7df8ec1e
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-25 17:09:03 +0300

    chore(angular): migrate from Angular 18 to 21 via ng update; fix standalone spec

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -3,9 +3,10 @@ import { type DxSpeechToTextTypes } from 'devextreme-angular/ui/speech-to-text';
 import notify from 'devextreme/ui/notify';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   speechRecognitionConfig = { continuous: true };
